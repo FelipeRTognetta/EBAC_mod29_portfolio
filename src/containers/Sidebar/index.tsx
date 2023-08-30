fe82d86edc3972ea0cad800ec0b7dc50e6d3f63e
Felipe R Tognetta <felrt@hotmail.com>
2023-08-30 15:00:16 -0300

    construindo a estrutura e a seção sobre mim

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/FelipeRTognetta.png" alt="Profile picture" />
+    <Titulo fontSize={20}>Felipe RT</Titulo>
+  </aside>
+)
+
+export default Sidebar
